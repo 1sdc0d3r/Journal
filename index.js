15367a2dee2f7141e4a2ebab6a81d7034d862a1a
Braden <fish2355@hotmail.com>
2020-03-05 21:24:37 -0700

    server start clg

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./server/server");
 const { PORT } = process.env || 5001;
 
 server.listen(PORT, () => {
-  console.log(`http://localhost:5000`);
+  console.log(`Listening on port ${PORT}`);
 });
