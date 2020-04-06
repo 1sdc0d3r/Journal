3c6c3dd56070762803b3946f94522f881ba09f35
Braden <fish2355@hotmail.com>
2020-04-06 12:52:20 -0600

    fixed action for heroku

--- client/src/Redux/actions/entry/submitAction.js
+++ client/src/Redux/actions/entry/submitAction.js
@@ -8,7 +8,7 @@ export const ENTRY_SUBMIT_FAIL = "ENTRY_SUBMIT_FAIL";
 export const submitAction = (entry, history) => (dispatch) => {
   dispatch({ type: ENTRY_SUBMIT_START });
   axiosWithAuth()
-    .post(`${address.HEROKUpi/entry`, entry)
+    .post(`${address.HEROKU}/entry`, entry)
     .then((res) => {
       dispatch({ type: ENTRY_SUBMIT_SUCCESS, payload: res.config.data });
       history.push("/journal");
